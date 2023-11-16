baabc1f926121b5ad7c0b0250e9643f60d1f7894
mmYM1037 <mmym7.3710@gmail.com>
2023-11-15 23:17:14 -0300

    Merge pull request #5 from nicolasjitorres/develop
    Agregando Link de Trello al readme y carpeta Diseños

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-// hola, estoy probando
-// este es otro comentario
+// Dependencias
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
+
+// Constantes
+const PUERTO = 3000;
+
+// Levantando el servidor
+app.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en el puerto ${PUERTO}...`);
+});
+
+// Ruta hacía index (ex home)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+// Ruta hacía el detalle del producto
+app.get('/productDetail', (req, res) => {
+  res.sendFile(path.join(__dirname, '.views\productDetail.html'));
+});
+
+// Ruta hacía el carrito de compras
+app.get('/productCart', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/productCart.html'));
+});
+
+// Ruta hacía el registro
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+// Ruta hacía el login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+
+// Ruta hacía index (ex home)
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
