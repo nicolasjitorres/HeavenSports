53502fc3699dd7811195f95945224f253cae1855
mmYM1037 <mmym7.3710@gmail.com>
2023-12-11 21:41:35 -0300

    fix feedback Sprint 3: diagramado de rutas de todos los metodos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,17 +3,30 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', ... );
+
+/*** GET ONE PRODUCT ***/ 
 // Ruta hacía el detalle del producto
-router.get('/detail', productController.detail);
+router.get('/detail:id', ... );
+
+/*** CREATE ONE PRODUCT ***/ 
+// Ruta hacía el formulario de carga de producto
+router.get('/formCarga', ... );
+router.post('/', upload.array(' ... '), ... ); 
+
+/*** EDIT ONE PRODUCT ***/ 
+// Ruta hacía el formulario de edicion de producto
+router.get('/formEdit/:id', ... );
+router.put('/:id', ... ); 
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/:id', ... ); 
+
+//////////////////////////////////////////////
 
 // Ruta hacía el carrito de compras
 router.get('/cart', productController.cart);
-
-// Ruta hacía el formulario de carga de producto
-router.get('/formCarga', productController.formCarga);
-
-// Ruta hacía el formulario de edicion de producto
-router.get('/formEdit', productController.formEdit);
 
 
 module.exports = router;
