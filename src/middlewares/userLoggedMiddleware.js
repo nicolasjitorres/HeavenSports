e04f4a70beee252fd94651ba09195c8601cc0062
mmYM1037 <mmym7.3710@gmail.com>
2024-01-21 18:30:21 -0300

    agregado de acceso a 'Lista de Usuarios' desde el menu hamburguesa para los administradores

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -4,6 +4,8 @@ const userLoggedMiddleware = (req, res, next)=>{
 
     let userEmailFromCookie = req.cookies.emailUserRemember;
     let userLogged = userService.findByField('Email', userEmailFromCookie);
+    let admin
+
     if (userLogged) {
         req.session.userLogged = {...userLogged};
         delete req.session.userLogged.Contrasena;
@@ -11,9 +13,14 @@ const userLoggedMiddleware = (req, res, next)=>{
 
     if(req.session.userLogged){
         res.locals.userLogged = true;
+        if(req.session.userLogged.Categoria == "Administrador"){
+            res.locals.admin = true
+        }
+
     }else{
         res.locals.userLogged = false;
     }
+
     next();
 }
 
