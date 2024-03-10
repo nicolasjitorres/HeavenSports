a8a5d5124a8f7e93460ce5cc1d41284c7f2e7a4b
mmYM1037 <mmym7.3710@gmail.com>
2024-03-10 16:12:45 -0300

    feature: validacion front 'addSize product'

--- src/middlewares/validations/ValCreateEditProduct.js
+++ src/middlewares/validations/ValCreateEditProduct.js
@@ -14,10 +14,18 @@ let ValCreateEditProduct = [
         .isDecimal().withMessage('Se debe ingresar un numero decimal.'),
     body('descuento')
         .notEmpty().withMessage('Si no se aplica descuento, completar con "0".')
-        .isInt().withMessage('Se debe ingresar un numero entero'),
+        .isInt().withMessage('Se debe ingresar un numero entero.'),
+    body('id_marca')
+        .notEmpty().withMessage('Se debe escoger una marca del listado.'),
+    body('id_color')
+        .notEmpty().withMessage('Se debe escoger un color del listado.'),
+    body('id_talle')
+        .notEmpty().withMessage('Se debe escoger un talle del listado.'),
     body('stock')
         .notEmpty().withMessage('Ingresar un numero para indicar el Stock de este producto.')
-        .isInt().withMessage('Se debe ingresar un numero entero'),
+        .isInt().withMessage('Se debe ingresar un numero entero.'),
+    body('categorias')
+        .notEmpty().withMessage('Se debe escoger una categoria.'),
 
     body('imagenes').custom((value, { req }) => {
         let file = req.file;
--- src/middlewares/validations/ValEditSizeProduct.js
+++ src/middlewares/validations/ValEditSizeProduct.js
@@ -3,6 +3,8 @@ const { body } = require('express-validator');
 const path = require('path');
 
 let ValEditSizeProduct = [
+    body('id_talle')
+        .notEmpty().withMessage('Se debe escoger un talle del listado.'),
     body('stock')
         .notEmpty().withMessage('Ingresar un numero para indicar el Stock de este producto.')
         .isInt().withMessage('Se debe ingresar un numero entero'),
