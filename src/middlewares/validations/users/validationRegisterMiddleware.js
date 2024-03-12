82f9948310dcbd0357b852624d71df00fa6fc3d2
Nicolas Torres <nicoignacio123@gmail.com>
2024-03-11 23:51:24 -0300

    Agregando las validaciones en el back

---
+++ src/middlewares/validations/users/validationRegisterMiddleware.js
@@ -0,0 +1,58 @@
+const {
+    body,
+    validationResult
+} = require('express-validator');
+const userService = require('../../../services/userService');
+
+const validationRegisterMiddleware = {
+    validation: [
+        body('nombre')
+        .notEmpty().withMessage('El campo nombre es obligatorio.').bail()
+        .isLength({
+            min: 2
+        }).withMessage('Debe tener como mínimo 2 caracteres.'),
+
+        body('apellido')
+        .notEmpty().withMessage('El campo apellido es obligatorio.').bail()
+        .isLength({
+            min: 2
+        }).withMessage('Debe tener como mínimo 2 caracteres.'),
+
+        body('email')
+        .notEmpty().withMessage('El campo email es obligatorio.').bail()
+        .isEmail().withMessage('Debe ser un email valido.').bail()
+        .custom(async (email) => {
+            const user = await userService.findByField('email', email);
+            if (user) {
+                throw new Error('El correo ya se encuentra registrado.')
+            }
+            return true;
+        }),
+
+        // PUEDO USAR isStrongPassword que superfunciona, se puede personalizar mas intuitivamente que matches. por ahora dejaremos ese
+        body('contrasena')
+        .notEmpty().withMessage('El campo contraseña es obligatorio.').bail()
+        .isLength({
+            min: 8
+        }).withMessage('Debe tener como mínimo 8 caracteres.').bail()
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/).withMessage('La contraseña debe incluir al menos una letra minúscula, una letra mayúscula, un número y un carácter especial.').bail()
+        .custom((contrasena, { req }) => {
+            if(contrasena != req.body.reContrasena){
+                throw new Error('Las contraseñas no coinciden.')
+            }
+            return true;
+        })
+    ],
+    result: (req, res, next) => {
+        const errors = validationResult(req);
+        if (errors.array().length) {
+            return res.render('users/register', {
+                errors: errors.array(),
+                usuario: req.body
+            });
+        }
+        next();
+    }
+}
+
+module.exports = validationRegisterMiddleware;
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,15 +4,19 @@ const productController = require('../controllers/productController');
 const adminMiddleware = require("../middlewares/adminMiddleware");
 const authUserMiddleware = require("../middlewares/authUserMiddleware");
 const multerMiddleware = require('../middlewares/multerMiddleware');
+const validationCreateMiddleware = require('../middlewares/validations/products/validationCreateMiddleware');
+const validationProductEditMiddleware = require('../middlewares/validations/products/validationProductEditMiddleware');
+const validationAddSizeMiddleware = require('../middlewares/validations/products/validationAddSizeMiddleware');
+const validationEditSizeMiddleware = require('../middlewares/validations/products/validationEditSizeMiddleware');
 const upload = multerMiddleware('products');
 
-/* GET ALL PRODUCTS */ 
-/* Obtener todos los productos */ 
-router.get('/', productController.index); 
+/* GET ALL PRODUCTS */
+/* Obtener todos los productos */
+router.get('/', productController.index);
 
-/* GET ONE PRODUCT */ 
+/* GET ONE PRODUCT */
 // Ruta hacía el detalle del producto
-router.get('/detail/:id', productController.detail); 
+router.get('/detail/:id', productController.detail);
 
 // Ruta hacia el carrito de compras del usuario
 router.get('/cart', authUserMiddleware, productController.cart);
@@ -20,19 +24,19 @@ router.get('/cart', authUserMiddleware, productController.cart);
 // Agrega un producto al carrito
 router.post('/cart/:id', productController.addCart);
 
-/* CREATE ONE PRODUCT */ 
-router.get('/create', adminMiddleware, productController.create); 
-router.post('/',adminMiddleware, upload.array('imagenes'), productController.save); 
+/* CREATE ONE PRODUCT */
+router.get('/create', adminMiddleware, productController.create);
+router.post('/create', adminMiddleware, upload.array('imagenes'), validationCreateMiddleware.validation, validationCreateMiddleware.result, productController.save);
 
-/* EDIT ONE PRODUCT */ 
-router.get('/edit/:id', adminMiddleware, productController.edit); 
-router.put('/:id',adminMiddleware, productController.update); 
+/* EDIT ONE PRODUCT */
+router.get('/edit/:id', adminMiddleware, productController.edit);
+router.put('/:id', adminMiddleware, validationProductEditMiddleware.validation, validationProductEditMiddleware.result, productController.update);
 
-/* DELETE ONE PRODUCT */ 
-router.delete('/:id', adminMiddleware, productController.logicDelete); 
+/* DELETE ONE PRODUCT */
+router.delete('/:id', adminMiddleware, productController.logicDelete);
 
 /* GET ALL IMAGES AND SIZES BY ONE PRODUCT */
-router.get('/edit/:id/relations', adminMiddleware, productController.relations)
+router.get('/edit/:id/relations', productController.relations)
 
 /* GET VIEW FOR ADD IMAGES TO ONE PRODUCT */
 router.get('/edit/:id/relations/images/create', adminMiddleware, productController.getAddImage);
@@ -47,13 +51,13 @@ router.delete('/edit/:id/relations/images/:idImagen', adminMiddleware, productCo
 router.get('/edit/:id/relations/sizes/create', adminMiddleware, productController.getAddSize);
 
 /* ADD SIZE TO ONE PRODUCT */
-router.post('/edit/:id/relations/sizes', adminMiddleware, productController.addSize);
+router.post('/edit/:id/relations/sizes', adminMiddleware, validationAddSizeMiddleware.validation, validationAddSizeMiddleware.result, productController.addSize);
 
 /* EDIT ONE SIZE OF ONE PRODUCT */
-router.get('/edit/:id/relations/sizes/:idTalle', adminMiddleware, productController.getEditSize);
+router.get('/edit/:id/relations/sizes/:idTalle', productController.getEditSize);
 
 /* EDIT ONE SIZE OF ONE PRODUCT */
-router.put('/edit/:id/relations/sizes/:idTalle', adminMiddleware, productController.editSize);
+router.put('/edit/:id/relations/sizes/:idTalle', validationEditSizeMiddleware.validation, validationEditSizeMiddleware.result, productController.editSize);
 
 /* DELETE ONE SIZE OF ONE PRODUCT */
 router.delete('/edit/:id/relations/sizes/:idTalle', adminMiddleware, productController.deleteSize);
